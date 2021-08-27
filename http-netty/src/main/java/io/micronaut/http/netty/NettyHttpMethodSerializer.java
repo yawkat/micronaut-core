f789ecbdbecf3c0dd8aa37a26568721735025c77
yawkat <jonas.konrad@oracle.com>
2021-08-27 08:49:34 +0200

    serializer for netty HttpMethod

---
+++ http-netty/src/main/java/io/micronaut/http/netty/NettyHttpMethodSerializer.java
@@ -0,0 +1,37 @@
+package io.micronaut.http.netty;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import io.micronaut.json.Deserializer;
+import io.micronaut.json.Serializer;
+import io.micronaut.json.SerializerLocator;
+import io.netty.handler.codec.http.HttpMethod;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+import java.io.IOException;
+
+/**
+ * Jackson-databind creates this serializer magically, we need to add our own
+ */
+@Singleton
+class NettyHttpMethodSerializer implements Serializer<HttpMethod>, Deserializer<HttpMethod> {
+    private final Serializer<? super String> stringSerializer;
+    private final Deserializer<? extends String> stringDeserializer;
+
+    @Inject
+    NettyHttpMethodSerializer(SerializerLocator locator) {
+        this.stringSerializer = locator.findContravariantSerializer(String.class);
+        this.stringDeserializer = locator.findInvariantDeserializer(String.class);
+    }
+
+    @Override
+    public HttpMethod deserialize(JsonParser decoder) throws IOException {
+        return HttpMethod.valueOf(stringDeserializer.deserialize(decoder));
+    }
+
+    @Override
+    public void serialize(JsonGenerator encoder, HttpMethod value) throws IOException {
+        stringSerializer.serialize(encoder, value.name());
+    }
+}
