467eb92748b29d2f230082497d6c10d1a995bee5
yawkat <jonas.konrad@oracle.com>
2021-09-20 08:15:31 +0200

    remove now-unused methods

--- serialization-generator/src/main/java/io/micronaut/json/generator/symbol/bean/ElementUtil.java
+++ serialization-generator/src/main/java/io/micronaut/json/generator/symbol/bean/ElementUtil.java
@@ -29,59 +29,6 @@ final class ElementUtil {
     private ElementUtil() {
     }
 
-    static boolean equals(ClassElement a, ClassElement b) {
-        return equals(a, b, 0);
-    }
-
-    private static boolean equals(ClassElement a, ClassElement b, int depth) {
-        // todo: mn3 .equals
-
-        // todo: hack: some ClassElements will recursively return the same types, e.g. class X<T extends X>.
-        //  there does not seem to be a way to handle this case properly with current API – we can't check the "path"
-        //  to the current call, because that would require an equals impl, which we are implementing here :)
-        if (depth > 10) {
-            return true;
-        }
-
-        if (!a.getName().equals(b.getName())) {
-            return false;
-        }
-        Map<String, ClassElement> aArgs = a.getTypeArguments();
-        Map<String, ClassElement> bArgs = b.getTypeArguments();
-        if (!aArgs.keySet().equals(bArgs.keySet())) {
-            return false;
-        }
-        for (String argument : aArgs.keySet()) {
-            if (!equals(aArgs.get(argument), bArgs.get(argument), depth + 1)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    static boolean equals(MethodElement a, MethodElement b) {
-        if (!equals(a.getDeclaringType(), b.getDeclaringType())) {
-            return false;
-        }
-        if (!a.getName().equals(b.getName())) {
-            return false;
-        }
-        if (!equals(a.getGenericReturnType(), b.getGenericReturnType())) {
-            return false;
-        }
-        ParameterElement[] paramsA = a.getParameters();
-        ParameterElement[] paramsB = b.getParameters();
-        if (paramsA.length != paramsB.length) {
-            return false;
-        }
-        for (int i = 0; i < paramsA.length; i++) {
-            if (!equals(paramsA[i].getGenericType(), paramsB[i].getGenericType())) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     static <T extends Annotation> AnnotationValue<T> getAnnotation(Class<T> type, AnnotatedElement one, Collection<AnnotatedElement> additional) {
         AnnotationValue<T> value = one.getAnnotation(type);
         if (value != null) {
