e3e845e317ecab5ada2aacc56e04e98ee10cee55
yawkat <jonas.konrad@oracle.com>
2021-09-27 10:16:01 +0200

    remove unused imports

--- inject/src/main/java/io/micronaut/inject/ast/ReflectClassElement.java
+++ inject/src/main/java/io/micronaut/inject/ast/ReflectClassElement.java
@@ -17,18 +17,10 @@ package io.micronaut.inject.ast;
 
 import io.micronaut.core.annotation.Internal;
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.core.annotation.Nullable;
 
 import java.lang.reflect.Array;
-import java.lang.reflect.GenericArrayType;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
-import java.lang.reflect.WildcardType;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
